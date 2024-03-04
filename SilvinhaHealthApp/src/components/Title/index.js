1d4c33c9ce9ffc70acc34485bccca5fedb1f2cbf
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-03-04 12:23:53 -0300

    Foi terminado o title e começando o Form

--- SilvinhaHealthApp/src/components/Title/index.js
+++ SilvinhaHealthApp/src/components/Title/index.js
@@ -3,9 +3,9 @@ import { View, Text } from "react-native";
 import { styles } from './style';
 
 export default function Title() {
-    return(
-    <View style={styles.container}>
-     <Text style={styles.title}>Silvinha Health App!</Text>       
-    </View>
+    return (
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>Silvinha Health App!</Text>       
+        </View>
     );
 }
